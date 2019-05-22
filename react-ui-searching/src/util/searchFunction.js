d262f1d2499d5f84aa7e59f4da6baa51f7244b3e
JizongL <2015rpro@gmail.com>
2019-05-22 11:28:35 -0400

    added search algo service compo

---
+++ react-ui-searching/src/util/searchFunction.js
@@ -0,0 +1,38 @@
+
+const SearchAlgo = {
+
+
+LinearSearch(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+},
+
+binarySearch(array, value, start, end) {
+  var start = start === undefined ? 0 : start;
+  var end = end === undefined ? array.length : end;
+
+  if (start > end) {
+      return -1;
+  }
+
+  const index = Math.floor((start + end) / 2);
+  const item = array[index];
+
+  console.log(start, end);
+  if (item === value) {
+      return index;
+  }
+  else if (item < value) {
+      return SearchAlgo.binarySearch(array, value, index + 1, end);
+  }
+  else if (item > value) {
+      return SearchAlgo.binarySearch(array, value, start, index - 1);
+  }
+  }
+}
+
+export default SearchAlgo
