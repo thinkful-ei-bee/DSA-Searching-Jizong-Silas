0d38928bbe8395ee0c97a7c3498720dcd8f7c024
JizongL <2015rpro@gmail.com>
2019-05-22 11:39:08 -0400

    data added to state and sorted

--- react-ui-searching/src/App.js
+++ react-ui-searching/src/App.js
@@ -1,13 +1,19 @@
 import React,{Component} from 'react';
 import './App.css';
 import SearchAlgo from './util/searchFunction'
+import data from './util/dataStore'
 
 class App extends Component {
+state = {
+ data:data.split(' ').map(num=>Number(num)).sort()
+}
 
-  
 
 render(){
-  SearchAlgo.LinearSearch()
+  
+  console.log(this.state.data,'test data')
+  
+
   return (
     <div className="App">
          
