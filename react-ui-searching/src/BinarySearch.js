0112b3518b4503bffc1b361e3f08a9726281e68b
Loxphordex <silasishallahan@gmail.com>
2019-05-22 09:01:11 -0700

    BinarySearch

---
+++ react-ui-searching/src/BinarySearch.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function BinarySearch(array, value, start, end) {
+  var start = start === undefined ? 0 : start;
+  var end = end === undefined ? array.length : end;
+
+  if (start > end) {
+    return -1;
+  }
+
+  const index = Math.floor((start + end) / 2);
+  const item = array[index];
+
+  console.log(start, end);
+  if (item == value) {
+    return index;
+  }
+  else if (item < value) {
+    return BinarySearch(array, value, index + 1, end);
+  }
+  else if (item > value) {
+    return BinarySearch(array, value, start, index - 1);
+  }
+}
+
+export default BinarySearch;
